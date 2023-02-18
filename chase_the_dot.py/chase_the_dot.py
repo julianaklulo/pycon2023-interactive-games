4835450fe011fafec7405c7a38d45fbe6120b90a
Juliana Karoline <julianaklulo@gmail.com>
2023-02-18 11:43:54 -0300

    Improve chase_the_dot example

--- chase_the_dot.py/chase_the_dot.py
+++ chase_the_dot.py/chase_the_dot.py
@@ -4,49 +4,102 @@ import random
 min_value = 0
 max_value = 4
 
-position_x = 4
-position_y = 4
-brightness_position = 5
+player_x = 2
+player_y = 2
 
-goal_x = 0
-goal_y = 0
-brightness_goal = 9
+dot_x = 0
+dot_y = 0
 
-display.set_pixel(position_x, position_y, brightness_position)
-display.set_pixel(goal_x, goal_y, brightness_goal)
+brightness_player = 5
+brightness_dot = 9
+
+playing = False
 
 
+def start_game():
+    global playing
+
+    playing = False
+    
+    display.set_pixel(player_x, player_y, 0)
+    display.set_pixel(dot_x, dot_y, 0)
+    
+    while not (button_a.is_pressed() and button_b.is_pressed()):
+        display.show(Image.DIAMOND)
+        sleep(350)
+        display.clear()
+        sleep(350)
+
+    display.set_pixel(player_x, player_y, brightness_player)
+    display.set_pixel(dot_x, dot_y, brightness_dot)
+    
+    playing = True
+
+
+def update_player_coordinates():
+    global player_x
+    global player_y
+    
+    accel_x = accelerometer.get_x()
+    accel_y = accelerometer.get_y()
+
+    if accel_x > 0:
+        player_x = player_x + 1 if player_x < max_value else max_value
+    elif accel_x < 0:
+        player_x = player_x - 1 if player_x > min_value else min_value
+        
+    if accel_y > 0:
+        player_y = player_y + 1 if player_y < max_value else max_value
+    elif accel_y < 0:
+        player_y = player_y - 1 if player_y > min_value else min_value
+
+
+def update_dot_coordinates():
+    global dot_x
+    global dot_y
+    
+    while dot_x == player_x and dot_y == player_y:
+        dot_x = random.randint(min_value, max_value)
+        if dot_x == 0:
+            dot_y = random.randint(min_value, max_value)
+        else:
+           dot_y = 0
+
+
+def flash_dot():
+    display.set_pixel(dot_x, dot_y, 0)
+    sleep(150)
+    display.set_pixel(dot_x, dot_y, brightness_dot)
+    sleep(150)
+    display.set_pixel(dot_x, dot_y, 0)
+    sleep(400)
+
+
+start_game()
 while True:
-    sleep(200)
-    display.set_pixel(position_x, position_y, 0)
-
-    x = accelerometer.get_x()
-    y = accelerometer.get_y()
-
-    if x > 0:
-        position_x = position_x + 1 if position_x < max_value else max_value
-    elif x < 0:
-        position_x = position_x - 1 if position_x > min_value else min_value
-        
-    if y > 0:
-        position_y = position_y + 1 if position_y < max_value else max_value
-    elif y < 0:
-        position_y = position_y - 1 if position_y > min_value else min_value
-
-    if position_x == goal_x and position_y == goal_y:
-        display.set_pixel(goal_x, goal_y, 0)
+    if button_a.was_pressed():
+        playing = False
         sleep(150)
-        display.set_pixel(goal_x, goal_y, brightness_goal)
-        sleep(150)
-        display.set_pixel(goal_x, goal_y, 0)
-        sleep(400)
 
-        while goal_x == position_x and goal_y == position_y:
-            goal_x = random.randint(min_value, max_value)
-            if goal_x == 0:
-                goal_y = random.randint(min_value, max_value)
-            else:
-                goal_y = 0
+    if button_b.was_pressed():
+        playing = True
+        sleep(150)
+
+    if button_a.is_pressed() and button_b.is_pressed():
+        display.set_pixel(dot_x, dot_y, 0)
+        display.set_pixel(player_x, player_y, 0)
+        sleep(200)
+        start_game()
         
-        display.set_pixel(goal_x, goal_y, brightness_goal)
-    display.set_pixel(position_x, position_y, brightness_position)
+    if playing:
+        sleep(200)        
+        display.set_pixel(player_x, player_y, 0)
+        
+        update_player_coordinates()
+        
+        if player_x == dot_x and player_y == dot_y:
+            flash_dot()
+            update_dot_coordinates()
+            display.set_pixel(dot_x, dot_y, brightness_dot)
+
+        display.set_pixel(player_x, player_y, brightness_player)
