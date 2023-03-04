3125fad5fd7ed987a2b5834bdf3880aed4435240
Juliana Karoline <julianaklulo@gmail.com>
2023-03-03 23:28:53 -0300

    Update genius to use OOP

--- genius/genius.py
+++ genius/genius.py
@@ -1,84 +1,130 @@
 from microbit import *
 import random
 
-sequence = []
-input = []
 
-
-def start_game():
-    while not (button_a.is_pressed() and button_b.is_pressed()):
-        display.show(Image.DIAMOND)
-        sleep(350)
-        display.clear()
-        sleep(350)
-
-    for number in ("54321"):
-        display.show(number)
-        sleep(1000)
-
-
-def show_sequence():
-    directions = [Image.ARROW_E, Image.ARROW_W]
-    sequence.append(random.choice(directions))
-
-    for direction in sequence:
-        sleep(500)
-        display.show(direction)
-        sleep(500)
-        display.clear()
+class Game():
+    def __init__(self):
+        self.sequence = Sequence()
+        self.input = Input()
     
-    display.show(Image('55555:'
-                       '55555:'
-                       '55555:'
-                       '55555:'
-                       '55555'))
-    sleep(350)
-    display.clear()
-    
-
-def read_input():
-    direction_pressed = None
-    
-    while len(input) < len(sequence):
-        if button_a.is_pressed():
-            direction_pressed = Image.ARROW_W
-            sleep(100)
-
-        elif button_b.is_pressed():
-            direction_pressed = Image.ARROW_E
-            sleep(100)
-
-        if direction_pressed:
-            display.show(direction_pressed)
-            sleep(250)
+    def start(self):
+        while not (button_a.is_pressed() and button_b.is_pressed()):
+            display.show(Image.TARGET)
+            sleep(350)
             display.clear()
-            
-            index = len(input)
-            input.append(direction_pressed)
-            if sequence[index] != direction_pressed:
-                break
-            direction_pressed = None
+            sleep(350)
+
+        for number in ("54321"):
+            display.show(number)
+            sleep(1000)
+
+    def end(self):
+        display.show(Image.SAD)
+        sleep(1500)
+        self.input.clear()
+        self.sequence.clear()
+
+    def check(self):
+        if self.input.check(self.sequence.sequence):
+            display.show(Image.HAPPY)
+            sleep(1000)
+            display.clear()
+            self.input.clear()
+            return True
+        return False
+
+    def run(self):
+        self.start()
+        while True:
+            self.sequence.show()
+            correct_answer = self.check()
+            if not correct_answer:
+                self.end()
+                self.start()
+
+
+class Sequence():
+    def __init__(self):
+        self.sequence = []
+        self.directions = [Image.ARROW_N, Image.ARROW_S, Image.ARROW_W, Image.ARROW_E]
+
+    def show(self):
+        self.sequence.append(random.choice(self.directions))
+
+        for direction in self.sequence:
+            sleep(500)
+            display.show(direction)
+            sleep(500)
+            display.clear()
         
+        display.show(Image('99999:99999:99999:99999:99999'))
+        sleep(350)
+        display.clear()
 
-def show_game_over():
-    display.show(Image.SAD)
-    sleep(1500)
-    sequence.clear()
-    input.clear()
-
-
-def show_correct_answer():
-    display.show(Image.HAPPY)
-    sleep(1500)
-    input.clear()
+    def clear(self):
+        self.sequence.clear()
 
 
-start_game()
-while True:
-    show_sequence()
-    read_input()
-    if input != sequence:
-        show_game_over()
-        start_game()
-    else:
-        show_correct_answer()
+class Button():
+    def __init__(self, pin):
+        self.pin = pin
+        self.previous_state = False
+    
+    def is_pressed(self):
+        return self.pin.read_digital() == 0
+
+    def was_pressed(self):
+        new_state = self.is_pressed()
+        was_pressed = new_state and not self.previous_state
+        self.previous_state = new_state
+        return was_pressed
+
+
+class Input():
+    def __init__(self):
+        self.input = []
+        self.direction_pressed = None
+        
+        self.button_N = Button(pin13)
+        self.button_S = Button(pin15)
+        self.button_W = Button(pin12)
+        self.button_E = Button(pin14)
+
+    def read(self):
+        if self.button_N.is_pressed():
+            self.direction_pressed = Image.ARROW_N
+            sleep(200)
+
+        elif self.button_S.is_pressed():
+            self.direction_pressed = Image.ARROW_S
+            sleep(200)
+
+        elif self.button_W.is_pressed():
+            self.direction_pressed = Image.ARROW_W
+            sleep(200)
+        
+        elif self.button_E.is_pressed():
+            self.direction_pressed = Image.ARROW_E
+            sleep(200)
+        
+        else:
+            self.direction_pressed = None
+
+        if self.direction_pressed:
+            self.input.append(self.direction_pressed)
+            display.show(self.direction_pressed)
+            sleep(200)
+            display.clear()
+
+    def check(self, sequence):
+        while len(self.input) < len(sequence):
+            self.read()
+            if sequence[:len(self.input)] != self.input:
+                return False
+        return True
+
+    def clear(self):
+        self.input.clear()
+
+game = Game()
+game.run()
