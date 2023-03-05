9cfe542685ee964291858475e582875eec58455b
Juliana Karoline <julianaklulo@gmail.com>
2023-03-05 13:46:47 -0300

    Improve chase the dot to use OOP

--- chase_the_dot.py/chase_the_dot.py
+++ chase_the_dot.py/chase_the_dot.py
@@ -1,105 +1,143 @@
 from microbit import *
 import random
-
-min_value = 0
-max_value = 4
-
-player_x = 2
-player_y = 2
-
-dot_x = 0
-dot_y = 0
-
-brightness_player = 5
-brightness_dot = 9
-
-playing = False
+import music
 
 
-def start_game():
-    global playing
+DISPLAY_MIN = 0
+DISPLAY_MAX = 4
 
-    playing = False
+
+class Game:
+    def __init__(self):
+        self.player = Player()
+        self.dot = Dot()
+        self.playing = False
+
+    def start(self):
+        display.show(Image.TARGET)
+        music.play(music.ENTERTAINER)
+        while not (button_a.is_pressed() and button_b.is_pressed()):
+            display.clear()
+            sleep(350)
+            display.show(Image.TARGET)
+            sleep(350)
+
+        for number in ("321"):
+            display.show(number)
+            music.play(music.BA_DING)
+            sleep(1000)
     
-    display.set_pixel(player_x, player_y, 0)
-    display.set_pixel(dot_x, dot_y, 0)
+        music.play(music.JUMP_UP)
+        self.playing = True
+
+    def check_pause(self):
+        if button_a.was_pressed():
+            self.playing = False
+            sleep(150)
+        if button_b.was_pressed():
+            self.playing = True
+            sleep(150)
     
-    while not (button_a.is_pressed() and button_b.is_pressed()):
-        display.show(Image.DIAMOND)
-        sleep(350)
+    def check_collision(self):
+        if self.player.x == self.dot.x and self.player.y == self.dot.y:
+            return True
+        return False
+
+    def run(self):
+        self.start()
         display.clear()
-        sleep(350)
-
-    display.set_pixel(player_x, player_y, brightness_player)
-    display.set_pixel(dot_x, dot_y, brightness_dot)
-    
-    playing = True
-
-
-def update_player_coordinates():
-    global player_x
-    global player_y
-    
-    accel_x = accelerometer.get_x()
-    accel_y = accelerometer.get_y()
-
-    if accel_x > 0:
-        player_x = player_x + 1 if player_x < max_value else max_value
-    elif accel_x < 0:
-        player_x = player_x - 1 if player_x > min_value else min_value
+        self.player.draw()
+        self.dot.draw()
         
-    if accel_y > 0:
-        player_y = player_y + 1 if player_y < max_value else max_value
-    elif accel_y < 0:
-        player_y = player_y - 1 if player_y > min_value else min_value
-
-
-def update_dot_coordinates():
-    global dot_x
-    global dot_y
+        while True:
+            self.check_pause()
+            if self.playing:
+                sleep(200)
+                self.player.clear()
+                self.player.update_coordinates()
+                if self.check_collision():
+                    music.play(music.POWER_UP)
+                    self.dot.flash()
+                    while self.dot.x == self.player.x and self.dot.y == self.player.y:
+                        self.dot.update_coordinates()
+                    self.dot.draw()
+                self.player.draw()
     
-    while dot_x == player_x and dot_y == player_y:
-        dot_x = random.randint(min_value, max_value)
-        if dot_x == 0:
-            dot_y = random.randint(min_value, max_value)
+
+class Player:
+    def __init__(self):
+        self.x = 2
+        self.y = 4
+        self.brightness = 5
+        self.button = pin15
+        self.mode = "accelerometer"
+
+    def set_mode(self):
+        if self.button.read_digital() == 0:
+            self.mode = "joystick"
         else:
-           dot_y = 0
+            self.mode = "accelerometer"
+    
+    def update_coordinates(self):
+        self.set_mode()
+        if self.mode == "accelerometer":
+            accel_x = accelerometer.get_x()
+            accel_y = accelerometer.get_y()
+    
+            if accel_x > 0:
+                self.x = self.x + 1 if self.x < DISPLAY_MAX else DISPLAY_MAX
+            elif accel_x < 0:
+                self.x = self.x - 1 if self.x > DISPLAY_MIN else DISPLAY_MIN
+            
+            if accel_y > 0:
+                self.y = self.y + 1 if self.y < DISPLAY_MAX else DISPLAY_MAX
+            elif accel_y < 0:
+                self.y = self.y - 1 if self.y > DISPLAY_MIN else DISPLAY_MIN
+
+        elif self.mode == "joystick":
+            joystick_x = pin1.read_analog()
+            joystick_y = pin2.read_analog()
+
+            if joystick_x > 600:
+                self.x = self.x + 1 if self.x < DISPLAY_MAX else DISPLAY_MAX
+            elif joystick_x < 400:
+                self.x = self.x - 1 if self.x > DISPLAY_MIN else DISPLAY_MIN
+                
+            if joystick_y < 400:
+                self.y = self.y + 1 if self.y < DISPLAY_MAX else DISPLAY_MAX
+            elif joystick_y > 600:
+                self.y = self.y - 1 if self.y > DISPLAY_MIN else DISPLAY_MIN
+
+    def clear(self):
+        display.set_pixel(self.x, self.y, 0)
+
+    def draw(self):
+        display.set_pixel(self.x, self.y, self.brightness)
 
 
-def flash_dot():
-    display.set_pixel(dot_x, dot_y, 0)
-    sleep(150)
-    display.set_pixel(dot_x, dot_y, brightness_dot)
-    sleep(150)
-    display.set_pixel(dot_x, dot_y, 0)
-    sleep(400)
+class Dot:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.brightness = 9
 
+    def get_coordinates(self):
+        return (self.x, self.y)
 
-start_game()
-while True:
-    if button_a.was_pressed():
-        playing = False
+    def update_coordinates(self):
+        self.x = random.randint(DISPLAY_MIN, DISPLAY_MAX)
+        self.y = random.randint(DISPLAY_MIN, DISPLAY_MAX)
+        
+    def flash(self):
+        self.clear()
         sleep(150)
-
-    if button_b.was_pressed():
-        playing = True
+        self.draw()
         sleep(150)
+        self.clear()
+        sleep(400)
 
-    if button_a.is_pressed() and button_b.is_pressed():
-        display.set_pixel(dot_x, dot_y, 0)
-        display.set_pixel(player_x, player_y, 0)
-        sleep(200)
-        start_game()
-        
-    if playing:
-        sleep(200)        
-        display.set_pixel(player_x, player_y, 0)
-        
-        update_player_coordinates()
-        
-        if player_x == dot_x and player_y == dot_y:
-            flash_dot()
-            update_dot_coordinates()
-            display.set_pixel(dot_x, dot_y, brightness_dot)
+    def clear(self):
+        display.set_pixel(self.x, self.y, 0)
 
-        display.set_pixel(player_x, player_y, brightness_player)
+    def draw(self):
+        display.set_pixel(self.x, self.y, self.brightness)
